6553793206009162608c71098eee5aa2cf238b0e
Malekchairat <malek.chairat@esprit.tn>
2026-02-01 16:54:30 +0100

    Sponsor entity

---
+++ src/main/java/tn/esprit/ds/championnat/entities/Sponsor.java
@@ -0,0 +1,71 @@
+package tn.esprit.ds.championnat.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Sponsor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idSponsor;
+
+    private String nom;
+
+    private String pays;
+
+    private Float budgetAnnuel;
+
+    private Boolean bloquerContrat;
+
+    // Constructors
+    public Sponsor() {
+    }
+
+    public Sponsor(String nom, String pays, Float budgetAnnuel, Boolean bloquerContrat) {
+        this.nom = nom;
+        this.pays = pays;
+        this.budgetAnnuel = budgetAnnuel;
+        this.bloquerContrat = bloquerContrat;
+    }
+
+    // Getters & Setters
+    public Long getIdSponsor() {
+        return idSponsor;
+    }
+
+    public void setIdSponsor(Long idSponsor) {
+        this.idSponsor = idSponsor;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPays() {
+        return pays;
+    }
+
+    public void setPays(String pays) {
+        this.pays = pays;
+    }
+
+    public Float getBudgetAnnuel() {
+        return budgetAnnuel;
+    }
+
+    public void setBudgetAnnuel(Float budgetAnnuel) {
+        this.budgetAnnuel = budgetAnnuel;
+    }
+
+    public Boolean getBloquerContrat() {
+        return bloquerContrat;
+    }
+
+    public void setBloquerContrat(Boolean bloquerContrat) {
+        this.bloquerContrat = bloquerContrat;
+    }
+}
